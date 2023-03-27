7dbb6cd7fe45f9709184829f3e13137ef134b3fa
unknown <sagarmikeylevi@gmail.com>
2023-03-27 21:07:58 +0530

    Learn about useEffect cleanup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,19 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+
+    const identifier = setTimeout(() => {
+      console.log('Checking form validity');
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    } , 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
+    
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
